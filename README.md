ea10072f376ee634147eabd55531b039e1cf5a01
Paulo Augusto <paulo-augusto12@outlook.com>
2023-01-30 10:10:26 -0300

    feat: desenvolvidos os dois primeiros requisitos e suas respectivas regras de negócio

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Requisitos
-- [ ] Deve ser possível criar uma conta <br>
-- [ ] Deve ser possível buscar o extrato bancário do cliente <br>
+- [X] Deve ser possível criar uma conta <br>
+- [x] Deve ser possível buscar o extrato bancário do cliente <br>
 - [ ] Deve ser possível realizar um depósito <br>
 - [ ] Deve ser possível realizar um saque <br>
 - [ ] Deve ser possível buscar o extrato bancário do cliente por data <br>
@@ -9,9 +9,9 @@
 - [ ] Deve ser possível deletar uma conta <br>
 
 ### Regras de negócio
-- [ ] Não deve ser possível cadastrar uma conta com CPF já existente <br>
+- [x] Não deve ser possível cadastrar uma conta com CPF já existente <br>
 - [ ] Não deve ser possível fazer depósito em uma conta não existente <br>
-- [ ] Não deve ser possível buscar extrato em uma conta não existente <br>
+- [x] Não deve ser possível buscar extrato em uma conta não existente <br>
 - [ ] Não deve ser possível fazer saque em uma conta não existente <br>
 - [ ] Não deve ser possível excluir uma conta não existente <br>
 - [ ] Não deve ser possível fazer saque quando o saldo for insuficiente <br>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
-const express = require("express")
-
+const express = require("express");
+const crypto = require("node:crypto");
 
 const app = express();
 
+app.use(express.json());
+
+const customers = [];
+
+// Criação de um cliente
+
+app.post("/accounts", (req, res) => {
+  const { name, cpf } = req.body;
+
+  const existentCustomer = customers.some((customer) => customer.cpf === cpf);
+
+  if (existentCustomer) {
+    return res.status(400).json({ ERRO: "Usuário já existente" });
+  }
+
+  const id = crypto.randomUUID();
+
+  customers.push({
+    name,
+    cpf,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+
+app.get("/accounts", (req, res) => {
+  res.status(200).send(customers);
+});
+
+// listar um extrato de um cliente
+
+app.get("/statement/:cpf", (req, res) => {
+  const { cpf } = req.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res
+      .status(400)
+      .json({ ERRO: "O cliente informado não possui uma conta" });
+  }
+  return res.json(customer.statement);
+});
 
 app.listen(3333);
